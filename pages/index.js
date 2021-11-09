db8e732b76075ebf8ceb4beddd4cab52d9e84a1d
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2021-11-09 18:11:34 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,6 +21,25 @@ export default function Home({ posts }) {
       <PageSEO title={siteMetadata.title} description={siteMetadata.description} />
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
+          <h1 class="py-14 text-[23vw] sm:text-10xl leading-none select-none tracking-tightest font-extrabold text-center">
+            <span class="relative block">
+              <span class="px-2">
+                Develop.
+              </span>
+            </span>
+            <span class="relative block">
+              <span class="px-2">
+                Preview.
+              </span>
+            </span>
+            <span class="relative block">
+              <span class="px-2">
+                Ship.
+              </span>
+            </span>
+          </h1>
+          
+          
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Latest
           </h1>
