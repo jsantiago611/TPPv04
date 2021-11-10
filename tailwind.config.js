dc3c3d18ca01c7874ec53253f6869f5ca3c3d921
Jonathan Santiago <36797222+jsantiago611@users.noreply.github.com>
2021-11-10 14:24:29 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -89,12 +89,12 @@ module.exports = {
         },
       },
       animation: {
-        'gradient-background-1': 'gradient-background-1 8s infinite',
-        'gradient-foreground-1': 'gradient-foreground-1 8s infinite',
-        'gradient-background-2': 'gradient-background-2 8s infinite',
-        'gradient-foreground-2': 'gradient-foreground-2 8s infinite',
-        'gradient-background-3': 'gradient-background-3 8s infinite',
-        'gradient-foreground-3': 'gradient-foreground-3 8s infinite',
+        'gradient-background-1': 'gradient-background-1 7s infinite',
+        'gradient-foreground-1': 'gradient-foreground-1 7s infinite',
+        'gradient-background-2': 'gradient-background-2 7s infinite',
+        'gradient-foreground-2': 'gradient-foreground-2 7s infinite',
+        'gradient-background-3': 'gradient-background-3 7s infinite',
+        'gradient-foreground-3': 'gradient-foreground-3 7s infinite',
       },
       typography: (theme) => ({
         DEFAULT: {
